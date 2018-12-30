2d7fc502b5920c4ce7e7c8ae7568ee132c3adb66
KuaqSon <qson.ng@gmail.com>
2018-12-30 12:28:39 +0700

    Added home component & layouting dashboard

--- internet-banking/src/App.js
+++ internet-banking/src/App.js
@@ -1,24 +1,30 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 
 import Dashboard from './pages/Dashboard/Dashboard';
 import Transactions from './pages/Transactions/Transactions';
 import Layout from './layout/Layout';
+import Home from './pages/Home/Home';
 
 class App extends Component {
   render() {
     return (
-      <Layout>
-        <Router>
-          <div>
-            <Route path="/dashboard" exact component={Dashboard} />
-            <Route path="/dashboard/transactions" exact component={Transactions} />
-          </div>
-        </Router>
-      </Layout>
+      <Router>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Layout>
+            <Router>
+              <div>
+                <Route exact path="/dashboard" component={Dashboard} />
+                <Route path="/dashboard/transactions" component={Transactions} />
+              </div>
+            </Router>
+          </Layout>
+        </Switch>
+      </Router>
     );
   }
 }
---
+++ internet-banking/src/pages/Home/Home.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class Home extends React.Component {
+  render() {
+
+    return (
+      <div>
+        home work!
+      </div>
+    );
+  }
+}
+
+export default Home;
+
