1906c430b01ea1903d7c9176772de40b4dd60bf5
KuaqSon <qson.ng@gmail.com>
2019-01-01 15:07:29 +0700

    Init login page

--- internet-banking/src/App.js
+++ internet-banking/src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
@@ -8,13 +7,15 @@ import Dashboard from './pages/Dashboard/Dashboard';
 import Transactions from './pages/Transactions/Transactions';
 import Layout from './layout/Layout';
 import Home from './pages/Home/Home';
+import Login from './pages/Login/Login';
 
 class App extends Component {
   render() {
     return (
       <Router>
         <Switch>
-          <Route exact path="/" component={Home} />
+          <Route exact path="/" render={({history}) => <Home history={history}/>} />
+          <Route exact path="/login" render={({history}) => <Login history={history}/>} />
           <Layout>
             <Route exact path="/dashboard" component={Dashboard} />
             <Route exact path="/dashboard/transactions" component={Transactions} />
