ffde5e9e737f35d1db0f00b6f2986fd4dbaefa20
KuaqSon <qson.ng@gmail.com>
2019-01-05 22:15:48 +0700

    Setup redux

--- internet-banking/src/index.js
+++ internet-banking/src/index.js
@@ -4,8 +4,23 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import 'semantic-ui-css/semantic.min.css';
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
+import reducers from './reducers';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+const store = createStore(
+    reducers,
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+    applyMiddleware(thunk)
+);
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>,
+    document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
