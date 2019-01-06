8f6e9758cfd6d199e7286e6f1b68043fad8f8e2b
KuaqSon <qson.ng@gmail.com>
2019-01-06 19:21:25 +0700

    Added close payment method modal

--- internet-banking/src/components/Payment/ListPayment.js
+++ internet-banking/src/components/Payment/ListPayment.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Grid, Dimmer, Loader } from 'semantic-ui-react';
+import { Grid, Dimmer, Loader, Modal, Button } from 'semantic-ui-react';
 import PaymentCard from './PaymentCard';
 import { fetchPayment } from '../../actions/payment';
 import { fetchUserData } from '../../actions/auth';
@@ -10,7 +10,12 @@ class ListPayment extends Component {
     super();
 
     this.state = {
-      loading: false
+      loading: false,
+      modalVisible: false,
+      modalSuccessVisible: false,
+      selectedNumber: '',
+      selectedBalance: '',
+      selectedId: ''
     };
   }
 
@@ -29,8 +34,58 @@ class ListPayment extends Component {
     });
   }
 
+  closeModal = () => this.setState({ modalVisible: false })
+  closeSuccess = () => {
+    this.setState({
+      modalSuccessVisible: false,
+      recharge: '',
+      selectedNumber: '',
+      selectedBalance: '',
+      selectedId: ''
+    });
+    this.loadPayment();
+  }
+
+  showModalDetail = (id, number, balance) => {
+    this.setState({
+      modalVisible: true,
+      selectedNumber: number,
+      selectedBalance: balance,
+      selectedId: id,
+    })
+  }
+
+  submitClosePayment = (id) => {
+    if (id === '') {
+      return;
+    }
+
+    this.setState({
+      loading: true,
+      modalVisible: false,
+      modalSuccessVisible: true
+    })
+
+    // this.props.rechargePayment({
+    //   accountId: id,
+    //   asset: this.state.recharge
+    // })
+    //   .then(data => {
+    //     this.setState({
+    //       loading: false
+    //     })
+    //   })
+    //   .catch(error => {
+    //     this.setState({
+    //       loading: false
+    //     })
+    //     console.log("fail")
+    //   });
+  }
+
   render() {
     const { payments, fetchPaymentsStatus } = this.props;
+    const { modalVisible, modalSuccessVisible, loading } = this.state;
 
     if (!fetchPaymentsStatus) {
       return (
@@ -40,19 +95,76 @@ class ListPayment extends Component {
       )
     } else {
       return (
-        <Grid>
-          <Grid.Row columns={4}>
-            {payments.map(p =>
-              <Grid.Column key={payments.indexOf(p)}>
-                  <PaymentCard
-                    index={payments.indexOf(p)}
-                    number={p.accountNumber}
-                    balance={p.asset > 0 ? p.asset : 0}
-                  />
-              </Grid.Column>
-            )}
-          </Grid.Row>
-        </Grid>
+        <div>
+          <Modal
+            dimmer="blurring"
+            size="tiny"
+            open={modalVisible}
+            closeOnDimmerClick={false}
+            onClose={this.closeModal}
+          >
+            <Modal.Header>Payment infor</Modal.Header>
+            <Modal.Content>
+              <div>
+                <p>Account number: <strong>{this.state.selectedNumber}</strong></p>
+                <p>Balance: <strong>{this.state.selectedBalance}</strong></p>
+              </div>
+            </Modal.Content>
+            <Modal.Actions>
+              <Button
+                onClick={() => this.submitClosePayment(this.state.selectedId)}
+                negative
+                labelPosition='right'
+                icon='close'
+                content='Close this payment account'
+              />
+            </Modal.Actions>
+          </Modal>
+
+          <Modal
+            dimmer="blurring"
+            size="tiny"
+            open={modalSuccessVisible}
+            closeOnDimmerClick={false}
+            onClose={this.closeSuccess}
+          >
+            <Modal.Header>Success!</Modal.Header>
+            <Modal.Content>
+              {loading ?
+                <Dimmer active inverted>
+                  <Loader inverted content='Loading' inline='centered' />
+                </Dimmer>
+                :
+                <p>Closed payment <strong>{this.state.selectedNumber}</strong></p>
+              }
+            </Modal.Content>
+            <Modal.Actions>
+              <Button
+                onClick={() => this.closeSuccess()}
+                positive
+                labelPosition='right'
+                icon='checkmark'
+                content='Ok'
+              />
+            </Modal.Actions>
+          </Modal>
+
+          <Grid>
+            <Grid.Row columns={4}>
+              {payments.map(p =>
+                <Grid.Column key={payments.indexOf(p)}>
+                  <div onClick={() => this.showModalDetail(p._id, p.accountNumber, p.asset)}>
+                    <PaymentCard
+                      index={payments.indexOf(p)}
+                      number={p.accountNumber}
+                      balance={p.asset > 0 ? p.asset : 0}
+                    />
+                  </div>
+                </Grid.Column>
+              )}
+            </Grid.Row>
+          </Grid>
+        </div>
       )
     }
   }
